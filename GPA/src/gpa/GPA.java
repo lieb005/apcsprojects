2b129a492ec0c13c61de4e7bd56cc15dc349fb44
period5 <period5@10.220.248.223>
2012-09-20 13:15:51 -0700

    initial commit

---
+++ GPA/src/gpa/GPA.java
@@ -0,0 +1,71 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gpa;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import javax.swing.JApplet;
+
+/**
+ *
+ * @author period5
+ */
+public class GPA extends JApplet
+{
+    HashMap <String, Byte[]> grades = new HashMap<String, Byte[]>();
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main (String[] args)
+    {
+	GPA gpa = new GPA ();
+	gpa.init ();
+	gpa.start ();
+    }
+
+    @Override
+    public void init ()
+    {
+	super.init ();
+	BufferedReader read = new BufferedReader (new InputStreamReader (System.in));
+	try
+	{
+	    
+	    while (true)
+	    {
+		System.out.println ("Enter the Name(s) separated by commas");
+		while (!read.ready ())
+		{
+		}
+		String[] names = read.readLine ().replaceAll("[ ]*,[ ]*", ",").split (",");
+		String[][] letterGrades = new String[names.length][];
+		for (int i = 0;i < names.length;i++)
+		{
+		    System.out.println ("Enter a comma separated list of letter grades for " + names[i]);
+		    while (!read.ready ())
+			    {}
+		    letterGrades[i] = read.readLine ().toLowerCase().replaceAll("[ ]*,[ ]*", ",").split (",");
+		    for (int j = 0;j < letterGrades[i].length;j++)
+		    {
+			
+		    }
+		}
+	    }
+	}
+	catch (IOException e)
+	{
+	}
+    }
+
+    @Override
+    public void start ()
+    {
+	super.start ();
+    }
+}
