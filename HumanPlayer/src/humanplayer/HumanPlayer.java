884b4f2559bb035fce53cb81c0eab2ae9c6e89fc
Mark <mark005@pacbell.net>
2012-12-05 13:21:18 +0000

    Added a whole bunch of player code

--- HumanPlayer/src/humanplayer/HumanPlayer.java
+++ HumanPlayer/src/humanplayer/HumanPlayer.java
@@ -4,18 +4,72 @@
  */
 package humanplayer;
 
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Critter;
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+import java.net.URL;
+import java.util.ArrayList;
+
 /**
  *
  * @author mark
  */
-public class HumanPlayer
+public class HumanPlayer extends Critter
 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main (String[] args)
+    private int count = 0;
+
+    public HumanPlayer ()
     {
-        // TODO code application logic here
+    }
+
+    @Override
+    public void makeMove (Location loc)
+    {
+        final Location location = loc;
+        moveTo (new Location (0, 0));
+        while (true)
+        {
+            new Runnable ()
+            {
+
+                @Override
+                public void run ()
+                {
+                    while (true)
+                    {
+                        if (count >= 2)
+                        {
+                            for (int i = 0; i < getActors ().size (); i++)
+                            {
+                                getActors ().set (i, new HumanPlayer ());
+                            }
+                        }
+                        else
+                        {
+                            ArrayList<Actor> actors = getGrid ().getNeighbors (getLocation ());
+                            for (int i = 0; i < actors.size (); i++)
+                            {
+                                count = count + (actors.remove (i) == null ? 0 : 1);
+                            }
+                        }
+                        makeMove (location);
+                    }
+                }
+            }.run ();
+        }
+    }
+
+    @Override
+    public void moveTo (Location newLocation)
+    {
+        //super.moveTo (newLocation);
+    }
+
+    @Override
+    public void putSelfInGrid (Grid<Actor> gr, Location loc)
+    {
+        //super.putSelfInGrid (gr, loc);
     }
 }
