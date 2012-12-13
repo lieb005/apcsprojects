8e5130ada67a70879c8bcd99c5f44ac3746c7bc6
root <root@10.220.245.22>
2012-12-13 13:39:53 -0800

    OMG PROGRESS (2)!

---
+++ HumanPlayer/src/humanplayer/HumanPlayerRunner.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package humanplayer;
+
+/**
+ *
+ * @author root
+ */
+public class HumanPlayerRunner {
+    public static void main(String[] args)
+    {
+        ActorWorld world = new ActorWorld();
+        HumanPlayer alice = new HumanPlayer(9);
+        HumanPlayer frank = new HumanPlayer(6);
+        frank.setColor(Color.darkGray);
+        alice.setColor(Color.PINK);
+        HumanPlayer bob = new HumanPlayer(3);
+        world.add(new Location(7, 8), alice);
+        world.add(new Location(5, 5), bob);
+        world.add(new Location(5, 8), frank);
+        world.show();
+    }
+}
