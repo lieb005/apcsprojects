a68082365a449e89362a867f437ac9aa56abffe1
Mark <mark005@pacbell.net>
2012-12-14 13:29:10 +0000

    say

--- HumanPlayer/src/humanplayer/HumanPlayer.java
+++ HumanPlayer/src/humanplayer/HumanPlayer.java
@@ -7,8 +7,11 @@ package humanplayer;
 import info.gridworld.actor.Actor;
 import info.gridworld.actor.Critter;
 import info.gridworld.grid.Location;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -61,7 +64,14 @@ public class HumanPlayer extends Critter
                     while(true)
                     //for (int j = 0;j < 64;j = (j+1))
                     {
-
+                        try
+                        {
+                            Runtime.getRuntime ().exec ("say TEST");
+                        }
+                        catch (IOException ex)
+                        {
+                            Logger.getLogger (HumanPlayer.class.getName()).log (Level.SEVERE, null, ex);
+                        }
                         //System.exit (1324253);
                         //console.print("lolllllol");
                         if (count > 2)
