c17a1a49f2030ba0d1c38bc1bcbc7539a7824704
root <root@10.220.245.22>
2012-12-17 11:32:19 -0800

    some chan \\\\

--- HumanPlayer/src/humanplayer/HumanPlayerRunner.java
+++ HumanPlayer/src/humanplayer/HumanPlayerRunner.java
@@ -19,10 +19,10 @@ public class HumanPlayerRunner
     public static void main (String[] args)
     {
         ActorWorld world = new ActorWorld ();
-        HumanPlayer alice = new HumanPlayer (9);
+        //HumanPlayer alice = new HumanPlayer (9);
         //ChildPlayer frank = new ChildPlayer (6);
         //frank.setColor (Color.darkGray);
-        alice.setColor (Color.PINK);
+        //alice.setColor (Color.PINK);
         HumanPlayer bob = new HumanPlayer (3);
         for (int i = 0; i < 20; i++)
         {
@@ -31,7 +31,7 @@ public class HumanPlayerRunner
                 world.add(new Critter ());
             }
         }
-        world.add (new Location (7, 8), alice);
+        //world.add (new Location (7, 8), alice);
         world.add (new Location (5, 5), bob);
         //world.add (new Location (5, 8), frank);
         world.show ();
