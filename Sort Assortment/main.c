16f17e2730030f82a8bae4b650af02d3f5507769
Mark <mark005@pacbell.net>
2013-02-13 12:39:57 -0800

    made Sort Assortment

---
+++ Sort Assortment/main.c
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.c
+ * Author: mark
+ *
+ * Created on 13. Februar 2013, 12:36
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+    
+    return (EXIT_SUCCESS);
+}
+
+double double* insertion(double double *insertion)
+{
+    return NULL;
+}
